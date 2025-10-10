f7f5a20a4eeb91f0babcd8f96cda379270e83725
fkruklyaramis <francois.krukly@aramis.group>
2025-10-10 10:42:45 +0200

    change test

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -90,7 +90,7 @@ class TestMainViews:
         """
         response = client.get(reverse('home'))
         assert response.status_code == 200
-        assert 'Welcome to Holiday Homes' in response.content.decode()
+        assert 'Welcome to Test' in response.content.decode()
         assert 'Profiles' in response.content.decode()
         assert 'Lettings' in response.content.decode()
 
